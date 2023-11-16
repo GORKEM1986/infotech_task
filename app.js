36209f15230ccc1a18f45ba4f0fd377b321a1f7c
GORKEM1986 <aslanakbey7106@gmail.com>
2023-11-16 15:09:32 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+// A-)
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 2 == 0) {
+    console.log(i);
+  }
+}
+
+
+
+
+
+
+//  B-)
+
+const name = "Fatih";
+const surname = "GORKEM";
+// const fullName = name + " " + surname; 1. yol buda calısır
+const fullName = name.concat(surname)
+console.log(fullName);
+
+
+
+
+
+
+
+
+
+
+
+
